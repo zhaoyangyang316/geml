c8ed40d9f6fef913e46bf7134612cb28a66260dd
Yangyang Zhao <crystal.luna@wavesandbox.com>
2013-01-06 05:25:47 -0500

    pca

--- data_process.py
+++ data_process.py
@@ -11,7 +11,7 @@ Created by YANGYANG ZHAO on 2013-01-03.
 ###
 # Tools
 ###
-import numpy
+from numpy import *
 import random
 import pylab
 import time
@@ -22,11 +22,28 @@ def process():
     print "Process to complete"
 
 def loaddata():
-    data = numpy.loadtxt('iris.txt')
+    data = loadtxt('iris.txt')
     return data
 
 def check():
     return True
 
-def normalisation():
-    print "normalisation to complete"
+def normalisation(X):
+    nb=X.shape[0]
+    mu= sum(X,axis=0)/nb
+    out=X-mu
+    ecart= (sum(out**2,axis=0)/nb)**0.5
+    for i in range(X.shape[1]):
+        if ecart[i]!=0:
+            out[:,i]=out[:,i]/ecart[i]
+    return out
+
+def pca(data,outDim):
+    X = mat(normalisation(data))
+    # compute the covariace matrix
+    S = ( X.T * X)/ X.shape[0]
+    U, O, V = linalg.svd(S, full_matrices=True)
+    O=diag(O[:outDim])**0.5
+    V=V[:outDim,:]
+    #V=V[:outDim,:]
+    return (mat(O)*V*X.T).T
